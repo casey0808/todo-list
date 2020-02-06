523ebd3c2f12563c6e35c958bf8ccd5ae8263e30
casey0808 <casey_c0808@outlook.com>
2020-02-06 10:36:05 +0800

    Updates

---
+++ precache-manifest.3c612d674e2afe1e23650aa8eebd42ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9195975758316f59260c7392b385a72f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "539d99d00ed383bbe427",
+    "url": "/todo-list/static/css/main.461ed4a3.chunk.css"
+  },
+  {
+    "revision": "7a626b252a3b149f8ecc",
+    "url": "/todo-list/static/js/2.c25429cc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list/static/js/2.c25429cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539d99d00ed383bbe427",
+    "url": "/todo-list/static/js/main.0217756b.chunk.js"
+  },
+  {
+    "revision": "3c8bc7aa72310e7d0f4e",
+    "url": "/todo-list/static/js/runtime-main.901ffa1a.js"
+  }
+]);
